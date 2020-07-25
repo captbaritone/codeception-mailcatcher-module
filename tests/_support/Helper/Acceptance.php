91086c43bf2935b0a1e0669beb7f8b1aab203c13
Marcelo Briones <ing@marcelobriones.com.ar>
2020-07-24 21:06:14 -0300

    Upgrades phpmailer

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,14 +1,23 @@
 <?php
+
 namespace Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 class Acceptance extends \Codeception\Module
 {
-    public function sendEmail(string $to, string $subject, string $body, bool $isHtml = false, ?string $encoding = null, array $attachments = [])
-    {
-        $phpmailer = new \PHPMailer();
+    public function sendEmail(
+        string $to,
+        string $subject,
+        string $body,
+        bool $isHtml = false,
+        ?string $encoding = null,
+        array $attachments = []
+    ): void {
+        $phpmailer = new PHPMailer();
         $phpmailer->isSMTP();
         $phpmailer->Host = '127.0.0.1';
         $phpmailer->Port = 1025;
@@ -20,9 +29,12 @@ class Acceptance extends \Codeception\Module
         }
 
         $phpmailer->addAddress($to);
+        $phpmailer->setFrom("sender@example.com");
         $phpmailer->Subject = $subject;
         $phpmailer->Body = $body;
         $phpmailer->isHTML($isHtml);
-        $phpmailer->send();
+        if (!$phpmailer->send()){
+            throw new \Exception($phpmailer->ErrorInfo);
+        };
     }
 }
